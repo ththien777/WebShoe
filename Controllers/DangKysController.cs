5d29cdf90d91b0a550f0643f7151848911c56813
ththien777 <ththien2001@gmail.com>
2021-06-30 20:07:58 +0700

    Initial commit

---
+++ Controllers/DangKysController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebShoe.Models;
+
+namespace WebShoe.Controllers
+{
+    public class DangKysController : Controller
+    {
+        private CT25Team25Entities db = new CT25Team25Entities();
+
+        // GET: DangKys
+        public ActionResult Index()
+        {
+            return View(db.KhachHangs.ToList());
+        }
+
+        // GET: DangKys/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhachHang khachHang = db.KhachHangs.Find(id);
+            if (khachHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khachHang);
+        }
+
+        // GET: DangKys/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DangKys/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaKH,TenKH,EMAIL,USERNAME,PASSWORD,RE_PASSWORD,GioiTinh,NgaySinh,DiaChi,PHONE")] KhachHang khachHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.KhachHangs.Add(khachHang);
+                db.SaveChanges();
+                return RedirectToAction("Index","Home");
+            }
+
+            return View(khachHang);
+        }
+
+        // GET: DangKys/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhachHang khachHang = db.KhachHangs.Find(id);
+            if (khachHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khachHang);
+        }
+
+        // POST: DangKys/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaKH,TenKH,EMAIL,USERNAME,PASSWORD,GioiTinh,NgaySinh,DiaChi,PHONE")] KhachHang khachHang)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(khachHang).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(khachHang);
+        }
+
+        // GET: DangKys/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            KhachHang khachHang = db.KhachHangs.Find(id);
+            if (khachHang == null)
+            {
+                return HttpNotFound();
+            }
+            return View(khachHang);
+        }
+
+        // POST: DangKys/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            KhachHang khachHang = db.KhachHangs.Find(id);
+            db.KhachHangs.Remove(khachHang);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/DangNhapController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebShoe.Models;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace WebShoe.Controllers
+{
+    public class DangNhapController : Controller
+    {
+       CT25Team25Entities db;
+        public DangNhapController()
+        {
+            db = new CT25Team25Entities();
+        }
+        // GET: DangNhap
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index(Models.KhachHang model)
+        {
+            using (var context = new CT25Team25Entities())
+            {
+                var account = context.KhachHangs.Where(acc => acc.USERNAME == model.USERNAME && acc.PASSWORD == model.PASSWORD).FirstOrDefault();
+                bool isValid = context.KhachHangs.Any(x => x.USERNAME == model.USERNAME
+                && x.PASSWORD == model.PASSWORD);
+                if (isValid)
+                {
+                    Session["MaKH"] = account.MaKH;
+                    Session["TenKH"] = account.TenKH;
+                    Session["USERNAME"] = account.USERNAME;
+                    Session["PASSWORD"] = account.PASSWORD;
+                    FormsAuthentication.SetAuthCookie(model.USERNAME, false);
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            ModelState.AddModelError("", "Invalid username or password !!!");
+            Session["Message"] = "Sai thông tin tài khoản hoặc mật khẩu !!!";
+            return View();
+        }
+        public ActionResult LogOut()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
